d3c0323a355fd9c62a015c0ff66c195ac7cfcb5e
kayhide <kayhide@gmail.com>
2019-06-23 15:22:44 +0900

    solve bash grep.

---
+++ grep/grep.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+set -eu
+
+grep_file() {
+    local pattern="$1"
+    local file="$2"
+
+    if $MATCH_ENTIRE_LINE; then
+        test_() {
+            [[ "$1" == "$pattern" ]]
+        }
+    else
+        test_() {
+            [[ "$1" == *"$pattern"* ]]
+        }
+    fi
+
+    if $IGNORE_CASE; then
+        pattern=${pattern,,}
+        test__() {
+            test_ "${@,,}"
+        }
+    else
+        test__() {
+            test_ "$@"
+        }
+    fi
+
+    if $INVERT_MATCH; then
+        test___() {
+            ! $(test__ "$@")
+        }
+    else
+        test___() {
+            test__ "$@"
+        }
+    fi
+
+
+    local i=0
+    while read line; do
+        i=$(( i + 1 ))
+        if $(test___ "$line"); then
+            if $SHOW_ONLY_FILENAME; then
+                echo "$file"
+                break
+            else
+                $SHOW_FILENAME && echo -n "$file:" || true
+                $SHOW_LINE_NUMBER && echo -n "$i:" || true
+                echo "$line"
+            fi
+        fi
+    done < "$file"
+}
+
+main() {
+    SHOW_LINE_NUMBER=false
+    IGNORE_CASE=false
+    SHOW_ONLY_FILENAME=false
+    MATCH_ENTIRE_LINE=false
+    INVERT_MATCH=false
+    while [[ "${1:-}" == "-"* ]]; do
+        case "$1" in
+            -n )
+                SHOW_LINE_NUMBER=true
+                shift
+                ;;
+            -i )
+                IGNORE_CASE=true
+                shift
+                ;;
+            -l )
+                SHOW_ONLY_FILENAME=true
+                shift
+                ;;
+            -x )
+                MATCH_ENTIRE_LINE=true
+                shift
+                ;;
+            -v )
+                INVERT_MATCH=true
+                shift
+                ;;
+            * )
+                echo "Unknown flag: $1"
+                exit 1
+                ;;
+        esac
+    done
+    local pattern="$1"
+    shift
+
+    if (( $# > 1 )); then
+        SHOW_FILENAME=true
+    else
+        SHOW_FILENAME=false
+    fi
+
+    for file in "$@"; do
+        grep_file "$pattern" "$file"
+    done
+}
+
+main "$@"
