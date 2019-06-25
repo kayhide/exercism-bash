ea1053d5f36c5ddf74d607286e69b1576ee1b6dd
kayhide <kayhide@gmail.com>
2019-06-25 10:27:03 +0900

    refactor bash raindrops.

--- raindrops/raindrops.sh
+++ raindrops/raindrops.sh
@@ -2,23 +2,20 @@
 
 set -eu
 
-is_divisible() {
-    if (( $1 % $2 == 0 )); then
-        echo true
-    else
-        echo false
-    fi
-}
-
 main() {
+    local num=$1
+    is_divisible_by() {
+        (( num % $1 == 0 ))
+    }
+
     local drops=""
-    if $(is_divisible $1 3); then
-        drops="Pling"
+    if is_divisible_by 3; then
+        drops+="Pling"
     fi
-    if $(is_divisible $1 5); then
+    if is_divisible_by 5; then
         drops+="Plang"
     fi
-    if $(is_divisible $1 7); then
+    if is_divisible_by 7; then
         drops+="Plong"
     fi
 
