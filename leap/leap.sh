37007ae43626a83374195efb98f8a3bf80955a98
kayhide <kayhide@gmail.com>
2019-06-26 00:47:46 +0900

    refactor bash reap.

--- leap/leap.sh
+++ leap/leap.sh
@@ -2,22 +2,20 @@
 
 set -eu
 
-is_divisible() {
-    if (( $1 % $2 == 0 )); then
-        echo true
-    else
-        echo false
-    fi
-}
-
 main() {
     if (( $# != 1 )) || [[ ! $1 =~ ^[0-9]+$ ]]; then
         echo 'Usage: leap.sh <year>'
         exit 1
     fi
 
-    if $(is_divisible $1 400) ||
-            ( ! $(is_divisible $1 100 ) && $(is_divisible $1 4) ); then
+
+    local num=$1
+    is_divisible_by() {
+        (( num % $1 == 0 ))
+    }
+
+    if is_divisible_by 400 ||
+            ( ! is_divisible_by 100 && is_divisible_by 4 ); then
         echo true
     else
         echo false
