a714f7cbf561b8563897fe73bc3b31b71974b4a1
kayhide <kayhide@gmail.com>
2019-07-14 00:43:49 +0900

    update bash binary-search.

--- binary-search/binary_search.sh
+++ binary-search/binary_search.sh
@@ -4,14 +4,13 @@ set -eu
 
 main() {
     local n=$1
-    shift
-    read -r -a xs <<< "$@"
+    read -r -a xs <<< "${@:2}"
+
     local lo=0
     local hi=${#xs[@]}
-    while (( lo + 1 < hi )); do
-        local i=$(( (hi - lo) / 2 + lo ))
-        (( ${xs[$i]} == n )) && lo=$i && break
-        (( ${xs[$i]} < n )) && lo=$i || hi=$i
+    while (( ${xs[$lo]} != n )) && (( lo + 1 < hi )); do
+        local i=$(( (lo + hi) / 2 ))
+        (( n < ${xs[$i]} )) && hi=$i || lo=$i
     done
     (( ${xs[$lo]} == n )) && echo $lo || echo -1
 }
