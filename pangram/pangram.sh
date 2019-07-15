9ab1a7b29adbc099ecbff1124070140913f85536
kayhide <kayhide@gmail.com>
2019-07-15 17:20:40 +0900

    solve bash pangram.

---
+++ pangram/pangram.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eu
+
+to_i() {
+    echo $(( $(printf "%d" "'$1") - 97 ))
+}
+
+main() {
+    local -i bits=0
+
+    while read -r -n1 char; do
+        char="${char,,}"
+        if [[ "$char" =~ [a-z] ]]; then
+            (( bits = bits | 1 << $(to_i "$char") )) || true
+        fi
+    done <<< "$1"
+
+    local -i full=$(( (1 << 26) - 1 ))
+    (( (bits & full) == full )) && echo true || echo false
+}
+
+main "$@"
