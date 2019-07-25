406c2ce352e515426ef9c35acf6aa618e1a2a105
kayhide <kayhide@gmail.com>
2019-07-26 00:57:35 +0900

    update bash bob.

--- bob/bob.sh
+++ bob/bob.sh
@@ -4,27 +4,22 @@ set -eu
 
 main() {
     local -i bits=0
-    while read -r -n1 char; do
-        if [[ $char == [[:lower:]] ]]; then
-            (( bits = bits & 7 | 1 ))
-        elif [[ $char == [[:upper:]] ]]; then
-            (( bits = bits & 7 | 2 ))
-        elif [[ $char == [[:digit:]] ]]; then
-            (( bits = bits & 7 | 4 ))
-        elif [[ $char == \? ]]; then
-            (( bits |= 8 ))
-        fi
-    done <<< "${1:-}"
+    if (( $# > 0 )); then
+        [[ $1 == *[[:lower:]]* ]] && (( bits |= 1 ))
+        [[ $1 == *[[:upper:]]* ]] && (( bits |= 2 ))
+        [[ $1 == *[[:digit:]]* ]] && (( bits |= 4 ))
+        [[ $1 =~ \?[[:space:]]*$ ]] && (( bits |= 8 ))
+    fi
 
     local res=""
-    res+=$(( bits >> 0 & 1 ))
-    res+=$(( bits >> 1 & 1 ))
-    res+=$(( bits >> 2 & 1 ))
     res+=$(( bits >> 3 & 1 ))
+    res+=$(( bits >> 2 & 1 ))
+    res+=$(( bits >> 1 & 1 ))
+    res+=$(( bits >> 0 & 1 ))
     case $res in
-        01*1 ) echo "Calm down, I know what I'm doing!" ;;
-        01** ) echo "Whoa, chill out!" ;;
-        ***1 ) echo "Sure." ;;
+        1*10 ) echo "Calm down, I know what I'm doing!" ;;
+        **10 ) echo "Whoa, chill out!" ;;
+        1*** ) echo "Sure." ;;
         0000 ) echo "Fine. Be that way!" ;;
         *    ) echo "Whatever." ;;
     esac
