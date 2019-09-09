ee6a4824d746a34e76517fd20512d1fd0e4619e5
kayhide <kayhide@gmail.com>
2019-09-09 22:56:02 +0900

    refactor bash phone-number.

--- phone-number/phone_number.sh
+++ phone-number/phone_number.sh
@@ -7,15 +7,12 @@ die() {
     exit 1
 }
 
-is_invalid() {
-    [[ ${#1} != 10 || $1 == [01]* || ${1:3} == [01]* ]]
-}
-
 main() {
     local num=${1//[^0-9]/}
-    [[ ${#num} == 11 && $num == 1* ]] && num=${num:1}
+    num=${num#1}
 
-    is_invalid "$num" && die "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9"
+    [[ ! $num =~ ^[2-9][0-9]{2}[2-9][0-9]{6}$ ]] &&
+        die "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9"
 
     echo "$num"
 }
